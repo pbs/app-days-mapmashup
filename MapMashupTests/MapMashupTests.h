8fbec70612ffa0888c46277df9259719ce037536
Rares Zehan <rares.zehan@threepillarglobal.com>
2012-02-24 10:39:02 +0200

    Initial Commit

---
+++ MapMashupTests/MapMashupTests.h
@@ -0,0 +1,13 @@
+//
+//  MapMashupTests.h
+//  MapMashupTests
+//
+//  Created by Rares Zehan on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MapMashupTests : SenTestCase
+
+@end
---
+++ MapMashupTests/MapMashupTests.m
@@ -0,0 +1,32 @@
+//
+//  MapMashupTests.m
+//  MapMashupTests
+//
+//  Created by Rares Zehan on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapMashupTests.h"
+
+@implementation MapMashupTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapMashupTests");
+}
+
+@end
